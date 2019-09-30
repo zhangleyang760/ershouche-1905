c759c096c0c0dde17f9e1306d73694c6d776403e
Edmond_J <1398752619@qq.com>
2019-09-30 11:45:07 +0800

    添加md5utils

---
+++ src/main/java/com/qf/utils/Md5Utils.java
@@ -0,0 +1,22 @@
+package com.qf.utils;
+
+import org.apache.shiro.crypto.hash.SimpleHash;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class Md5Utils {
+    public String getPasswordCode(String password,String name){
+
+        String algorithmName = "MD5";//加密算法
+        Object source = password;//要加密的密码
+
+        Object salt = name;//盐值，用用户名即可
+        int hashIterations = 1024;//加密次数
+        SimpleHash simpleHash = new SimpleHash(algorithmName,source,salt,hashIterations);
+        return simpleHash.toString();
+
+
+    }
+
+}
