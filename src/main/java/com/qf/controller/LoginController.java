bc82f12a1812b23b222a5844dd282975d6f5e572
qianfengdekubishenghuo <1227706999@qq.com>
2019-10-12 16:27:00 +0800

    V-IF初改

--- src/main/java/com/qf/controller/LoginController.java
+++ src/main/java/com/qf/controller/LoginController.java
@@ -9,28 +9,47 @@ import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.apache.shiro.subject.Subject;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
 
 
 @RestController
 public class LoginController {
 
     @RequestMapping("/login")
-    public String login(@RequestBody Seller seller){
-    System.out.println(seller);
-    try {
-        Subject subject=SecurityUtils.getSubject();
-        UsernamePasswordToken token=new UsernamePasswordToken(seller.getUsername(),seller.getPassword());
-        subject.login(token);
-        if (subject.isAuthenticated()){
-            return seller.getUsername();
+    public String login(@RequestBody Seller seller,HttpSession session){
+        String username=seller.getUsername();
+        String password=seller.getPassword();
+        if(username!=""&&username!=null&&password!=""&&password!=null) {
+            System.out.println(seller);
+            try {
+                Subject subject = SecurityUtils.getSubject();
+                UsernamePasswordToken token = new UsernamePasswordToken(seller.getUsername(), seller.getPassword());
+                subject.login(token);
+                if (subject.isAuthenticated()) {
+                    session.setAttribute("username",username);
+                    return seller.getUsername();
+                } else {
+                    return null;
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+                System.out.println("登录失败");
+            }
         }
-    }catch (Exception e){
-        e.printStackTrace();
-        System.out.println("登录失败");
-    }
-    return null;
+    return "值不能为空";
 }
+    /**
+     *
+     * @param session
+     * @return获取用户名
+     */
+    @RequestMapping(value = "/getUserSession",method = RequestMethod.POST)
+    public String getUserSession(HttpSession session){
+        return (String)session.getAttribute("username");
+    }
     @RequestMapping("/loginout")
     public String logout() {
         Subject subject = SecurityUtils.getSubject();
