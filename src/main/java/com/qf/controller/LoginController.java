5321ee14e70cdf983e99b55c7b75d9b5abab71b4
Edmond_J <1398752619@qq.com>
2019-10-12 15:53:16 +0800

    登录验证

--- src/main/java/com/qf/controller/LoginController.java
+++ src/main/java/com/qf/controller/LoginController.java
@@ -5,6 +5,7 @@ package com.qf.controller;
 import com.qf.domain.Seller;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.authz.annotation.RequiresAuthentication;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.apache.shiro.subject.Subject;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,6 +15,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LoginController {
+    @RequiresAuthentication
+    @RequestMapping("/checkLogin")
+    public String userInfo(){
+        return "成功";
+    }
 
     @RequestMapping("/login")
     public String login(@RequestBody Seller seller){
